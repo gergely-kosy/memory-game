98c13415b4801ecf32ffe0ca6bbaf9c2729776df
gergely-kosy <gergelykosy@gmail.com>
2023-07-15 20:43:29 +0200

    memory logic fully implemented, randomize missing

--- src/app/new-game/page.tsx
+++ src/app/new-game/page.tsx
@@ -7,29 +7,44 @@ import React, { useState } from "react";
 const NewGame = () => {
   const [previousNumberIndex, setPreviousNumberIndex] = useState<number | null>();
   const [foundNumberIndexes, setFoundNumberIndexes] = useState<number[]>([]);
+  const [guessNumberIndexes, setGuessNumberIndexes] = useState<number[]>([]);
 
   const numbers = [1,2,3,4,5,6,7,8];
   const finalNumbers = [...numbers, ...numbers];
 
 
   const checkMatchAndStore = (index: number) => {
-    if (previousNumberIndex === 0) {
-      setFoundNumberIndexes([...foundNumberIndexes, previousNumberIndex, index]);
-    } else if (previousNumberIndex && (finalNumbers[previousNumberIndex] === finalNumbers[index])) {
-      setFoundNumberIndexes([...foundNumberIndexes, previousNumberIndex, index]);
+    if (previousNumberIndex !== index) {
+      const previousNum = handleVisibility(index);
+  
+      if (!foundNumberIndexes.includes(index)) {
+        if ((previousNum === 0 || previousNum) && (finalNumbers[previousNum] === finalNumbers[index])) {
+          setFoundNumberIndexes([...foundNumberIndexes, previousNum, index]);
+        }
+        
+        setPreviousNumberIndex(index);
+      }
     }
-    
-    setPreviousNumberIndex(index);
+  }
+
+  const handleVisibility = (index: number) => {
+    if (guessNumberIndexes.length > 1) {
+      setGuessNumberIndexes([index]);
+      return null;
+    } else {
+      setGuessNumberIndexes([...guessNumberIndexes, index]);
+      return previousNumberIndex;
+    }
   }
 
 
   const handleShowNumbers = () => {
     return finalNumbers.map((number, index) => {
       const matched = foundNumberIndexes.includes(index);
-      console.log(foundNumberIndexes)
+      const isVisible = guessNumberIndexes.includes(index);
       
       return <div key={index} className="grid-item">
-      <SingleItem number={number} index={index} matched={matched} checkMatch={checkMatchAndStore} />
+      <SingleItem number={number} index={index} isVisible={isVisible} matched={matched} checkMatch={checkMatchAndStore} />
     </div>
     })
   }
--- src/components/SingleItem.tsx
+++ src/components/SingleItem.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 
-const SingleItem = ({ number, index, matched, checkMatch }: { number: number, index: number, matched: boolean, checkMatch: any }) => {
-    const [isVisible, setIsVisible] = useState(false);
+const SingleItem = ({ number, index, isVisible, matched, checkMatch }: 
+    { 
+        number: number, 
+        index: number, 
+        isVisible: boolean, 
+        matched: boolean, 
+        checkMatch: any 
+    }) => {
 
     const handleOnClick = () => {
-        setIsVisible(!isVisible);
         checkMatch(index);
     }
     return <button style={{ background: matched ? 'yellow' : '' }} onClick={() => handleOnClick()}>
-            {isVisible && number}
+            {matched ? number : isVisible && number}
         </button>
 }
 
